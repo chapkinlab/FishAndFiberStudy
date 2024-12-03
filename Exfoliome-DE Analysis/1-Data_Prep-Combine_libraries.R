9c4069785e15b8701844b9481fed5e5ef069d20f
Destiny Mullens <destiny_mullens@tamu.edu>
2024-12-03 15:23:10 -0600

    R-Code for DE Analysis

---
+++ Exfoliome-DE Analysis/1-Data_Prep-Combine_libraries.R
@@ -0,0 +1,79 @@
+## Lampe Fish & Fiber Study
+## This code is for combining all of the libraries for each sample.
+## Code last updated December 2nd, 2024 by Destiny Mullens
+
+library(dplyr)
+## Combine raw data from separate sequencing runs
+counts <- read.csv("../../Batch2/Lampe_FF_Batch2-optimized/summary/Lampe_FF_Batch2-optimized-counts.csv")
+
+counts2 <- read.csv("../../Batch3_resequenced-samples/Lampe_FF_Batch3-optimized/summary/Lampe_FF_Batch3-optimized-counts.csv", sep = "\t")[,c(2:9)] %>% 
+  setNames(.,c("Ensembl.ID","LFF.53061.S2.lib2","LFF.53061.S3_S145","LFF.53061.S6.lib1","LFF.53061.S6.lib2","LFF.53087.S3.lib1","LFF.53096.S6_S176","LFF.53126.S1.lib1")) %>% 
+  as.data.frame()
+
+counts3 <- read.csv("../../Batch4_resequenced-sample/Resequenced_Samples_Jan2024/summary/Resequenced_Samples_Jan2024-counts.csv", sep = "\t")[,c(2,4)] %>% 
+  setNames(.,c("Ensembl.ID","LFF.53002.S4.lib1"))
+
+## Removed re sequenced samples from original counts df
+counts <- counts %>% 
+  select(!any_of(names(counts2[2:8]))) %>% 
+  select(.,!any_of(names(counts3[2])))
+
+## Merge together original sequencing runs with new ones
+counts.new <- counts %>% 
+  left_join(.,counts2) %>% 
+  left_join(.,counts3)
+
+## Order samples by name
+counts.new <- counts.new[,order(names(counts.new))]
+
+## Fix names so that all samples have the same formatting with .lib# at the end
+## Get sample names
+sample.list <- names(counts.new) %>% as.data.frame() %>% setNames(.,c("Names"))
+## Separate Sample ID to get library information
+sample.list$library <- substring(sample.list$Names, 14, 18)
+## Replace those with S## with lib1
+sample.list$library[grep("^S",sample.list$library)] <- c('lib1')
+## Get the portion of sample name prior to .lib
+sample.list$Sample.Name <- substring(sample.list$Names, 1, 12)
+## Paste together Sample Name & corrected .lib information
+sample.list$New.Names <- paste(sample.list$Sample.Name,sample.list$library,sep = '.')
+## Fix these two so they don't have a '.' at the end
+sample.list$New.Names[1:2] <- c("Ensembl.ID","Gene.Name")
+## Repace names
+names(counts.new) <- sample.list$New.Names
+
+write.csv(counts.new,"../Working_Data/Counts/LFF-uncombined-raw-counts-all-batches-12022024.csv",row.names = FALSE)
+###########################################################
+## Combine libraries for each sample
+counts <- read.csv("../Working_Data/Counts/LFF-uncombined-raw-counts-all-batches-12022024.csv",sep = ',')
+## Get Gene Name & Ensembl.ID info for new data frame for combined libaries
+new.counts <- data.frame(counts[,1:2])
+
+## Create list of samples
+samples <- unique(substring(names(counts), 1, 12))
+
+for (i in 3:length(samples)) {
+  # Get column names that start with the current sample name
+  sample_cols <- grep(paste0('^', samples[i]), names(counts))
+  if(length(sample_cols)=='1'){
+    ## If there is only one library, that library is added to new data frame
+    sample.vector <- counts %>% select(.,all_of(sample_cols)) %>% setNames(.,c(paste(samples[i])))
+    new.counts <- cbind(new.counts,sample.vector)
+  } else {
+    ## If there are multiple libaries, they are then added together and added to new data frame
+    new.counts[[paste0(samples[i])]] <- rowSums(counts[,sample_cols])
+  }
+}
+
+## Check to make sure the counts match
+summary(rowSums(counts[,3:length(counts)])==rowSums(new.counts[,3:length(new.counts)]))
+
+## Remove samples that are dropped for analysis
+samples.to.drop <- c('LFF.53029','LFF.53048','LFF.53152','LFF.53096','LFF.53061')
+
+## Remove samples that will be dropped
+new.counts <- new.counts %>% dplyr::select(.,-starts_with(samples.to.drop))
+
+## Save data frame with combined libraries
+write.csv(new.counts,"../Working_Data/Counts/LFF-raw-counts-all-combined-12022024.csv",row.names = FALSE)
+saveRDS(new.counts,"../Working_Data/Counts/LFF-raw-counts-all-combined-12022024.rds")
---
+++ Exfoliome-DE Analysis/2-Filter-Normalize-DE-Analysis-endpoints.R
@@ -0,0 +1,99 @@
+## Lampe F&F Study
+## Filtering, Normalization & DE Analysis.
+## Last updated December 2, 2024 by Destiny Mullens
+
+library(dplyr)
+library(tibble)
+
+## Import counts
+counts <- read.csv("../Working_Data/Counts/LFF-raw-counts-all-combined-12022024.csv") %>% 
+  column_to_rownames(var = "Ensembl.ID") %>%              ## Move Ensembl ID's to row name
+  dplyr::select(.,-Gene.Name) %>%                         ## Remove column with Gene names
+  filter(.,rowSums(.)!=0) %>%                             ## Filter out genes with no counts in any sample
+  dplyr::select(-contains(c('.S1','.S2','.S4','.S5','NA')))    ## Remove all time points except endpoints (S3 & S6)
+
+
+## Import metadata
+meta <- readRDS("../Working_Data/LFF-Full_Metadata.rds") %>% 
+  filter(Sample.Name %in% names(counts)) %>%      ## Only import metadata for samples in counts df
+  dplyr::select(.,c(Sample.Name,Participant,Group))           ## Select only the Sample.Name & Group columns
+
+library(edgeR)
+## Convert counts to cpm for filtering
+counts.cpm <- counts %>% cpm() %>% as.data.frame()
+
+### Set cpm limit for required number of reads to keep a gene (over 20 cpm)
+filter.limit <- 20
+## Set number of samples in each group that must be at/over filter limit to keep gene
+sample.num <- 19
+
+## Count number of times a gene is over >= the filter limit
+features.count <- left_join(rownames_to_column(as.data.frame(t(counts.cpm))), meta,by=c("rowname"="Sample.Name")) %>%
+  group_by(Group) %>%                                                             ## Group by Group in metadata
+  dplyr::summarise(across(everything(), ~ sum(.x >= filter.limit))) %>%           ## Summarize and count number of samples with count >= filter limit
+  select_if(.,is.numeric) %>%                                                     ## Select numeric columns
+  t() %>% as.data.frame()                                                         ## transpose and make a df
+
+## Select Ensembl.IDs for any gene that that was over the filter limit at least the "sample.num" amount of times
+features.to.keep <- rownames(features.count)[features.count$V1 > sample.num] %>%  ## Select row names if more than sample num have gene for group 1
+  append(rownames(features.count)[features.count$V2 > sample.num]) %>%            ## Select row name if more than sample num have gene for group 2
+  unique()                                                                        ## Select unique Ensembl.ID's only
+
+## Keep genes meeting above criteria for analysis
+filtered.counts <- counts[features.to.keep,] %>%                                  ## Filter counts by keep only genes selected above
+  na.omit() %>% as.data.frame()                                                   ## Omit any NA's and make a dataframe
+
+## Save raw, filtered counts
+write.csv(filtered.counts,"../Working_Data/Counts/Raw-filtered-counts-endpoints-73percent-20cpm-12022024.csv")
+
+###################### DE Analysis ######################
+
+## Create DGE list with filtered counts and groups in meta data
+y <- DGEList(counts=filtered.counts, group = meta$Group)
+
+## Create another group in DGEList for Participant number
+y$samples$participant <- meta$Participant
+
+## Check samples to make sure everything looks okay
+y$samples
+
+## Create factored vector for participant number
+participant <- factor(meta$Participant)
+
+## Create factored vector for treatment group
+treatment <- factor(meta$Group,levels = c("B","A"))
+
+## Create design model for DE analysis 
+design <- model.matrix(~participant+treatment) %>% as.data.frame()
+rownames(design) <- colnames(y)
+
+## Check samples to make sure everything looks okay
+y$samples
+
+## Normalize counts
+y <- calcNormFactors(y, method = "TMM")
+
+## Extract normalized counts from DGEList
+TMM.norm <- cpm(y,log = FALSE) %>% round() %>% as.data.frame()
+
+## Save normalized counts for other analyses
+write.csv(TMM.norm,"../Working_Data/Counts/Norm-Counts-endpoints-73percent-20cpm-12022024.csv",row.names = TRUE)
+
+## Estimate dispersion
+y <- estimateDisp(y, design, robust = TRUE)
+
+## Fit model
+fitq <- glmQLFit(y, design)
+
+## Test for treatment effect
+qlf <- glmQLFTest(fitq)
+## View top results
+topTags(qlf,n=50)
+
+out1q <- topTags(qlf,n=nrow(filtered.counts),sort.by="PValue")$table
+out2q <- out1q[order(out1q$FDR),] # Sort by ascending FDR
+out2q$FC <- 2^(out1q$logFC)
+genes <- counts <- read.csv("../Working_Data/Counts/LFF-raw-counts-all-combined-12022024.csv")[,c(1:2)]
+out2q <- left_join(rownames_to_column(out2q,var = "Ensembl.ID"),genes)
+out2q <- out2q[,c("Ensembl.ID","Gene.Name","FC","logFC","logCPM","F","PValue","FDR")]
+write.csv(out2q,"LFF-paired_AvB-endpoints-QLF-12022024.csv")
